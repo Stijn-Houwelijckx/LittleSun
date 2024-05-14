b327d0e44e4340206aa5ba2318dd5b5e39f12b5a
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-05-14 17:19:53 +0200

    feat [sick leave]: Employees can now submit sick leave.

---
+++ classes/SickLeave.php
@@ -0,0 +1,93 @@
+<?php
+class SickLeave {
+    private $start_date;
+    private $end_date;
+    private $reason;
+
+    /**
+     * Get the value of start_date
+     */ 
+    public function getStart_date()
+    {
+        return $this->start_date;
+    }
+
+    /**
+     * Set the value of start_date
+     *
+     * @return  self
+     */ 
+    public function setStart_date($start_date)
+    {
+        $this->start_date = $start_date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of end_date
+     */ 
+    public function getEnd_date()
+    {
+        return $this->end_date;
+    }
+
+    /**
+     * Set the value of end_date
+     *
+     * @return  self
+     */ 
+    public function setEnd_date($end_date)
+    {
+        $this->end_date = $end_date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of reason
+     */ 
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    /**
+     * Set the value of reason
+     *
+     * @return  self
+     */ 
+    public function setReason($reason)
+    {
+        $this->reason = $reason;
+
+        return $this;
+    }
+
+    public function submitSickLeave(PDO $pdo, $user_id)
+    {
+        try {
+            $stmt = $pdo->prepare("INSERT INTO sick_leave (user_id, start_date, end_date, reason) VALUES (:user_id, :start_date, :end_date, :reason)");
+            $stmt->bindParam(':start_date', $this->start_date);
+            $stmt->bindParam(':end_date', $this->end_date);
+            $stmt->bindParam(':reason', $this->reason);
+            $stmt->bindParam(':user_id', $user_id);
+            if ($stmt->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            error_log('Database error: ' . $e->getMessage());
+            return false;
+        }
+    }
+
+    public static function getActiveSickLeave(PDO $pdo, $user_id)
+    {
+        $stmt = $pdo->prepare("SELECT * FROM sick_leave WHERE user_id = :user_id AND status = 1");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+}
--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,7 @@ include_once (__DIR__ . "/classes/TimeOffRequest.php");
 include_once (__DIR__ . "/classes/Task.php");
 include_once (__DIR__ . "/classes/TimeTracker.php");
 include_once (__DIR__ . "/classes/CalendarItem.php");
+include_once (__DIR__ . "/classes/SickLeave.php");
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -24,10 +25,11 @@ if (!isset($_SESSION["user_id"]) || $user["typeOfUser"] != "employee") {
 }
 
 $userRequests = TimeOffRequest::getRequestsByUserId($pdo, $user["id"]);
+$userSickLeave = SickLeave::getActiveSickLeave($pdo, $user["id"]);
 
 // Process time off request form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["reason"]) && isset($_POST["startdate"]) && isset($_POST["enddate"])) {
+    if ($_POST["typeOfForm"] == "time-off" && isset($_POST["reason"]) && isset($_POST["startdate"]) && isset($_POST["enddate"])) {
         $timeOffRequest = new TimeOffRequest();
 
         $timeOffRequest->setStart_date($_POST["startdate"]);
@@ -38,6 +40,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
         $timeOffRequest->submitRequest($pdo, $user["id"]);
+
+        header("Location: dashboard.php");
+    }
+
+    if ($_POST["typeOfForm"] == "sick-leave" && isset($_POST["reason"]) && isset($_POST["startdate"]) && isset($_POST["enddate"])) {
+        $sickLeave = new SickLeave();
+
+        $sickLeave->setStart_date($_POST["startdate"]);
+        $sickLeave->setEnd_date($_POST["enddate"]);
+        $sickLeave->setReason($_POST["reason"]);
+
+        $sickLeave->submitSickLeave($pdo, $user["id"]);
 
         header("Location: dashboard.php");
     }
@@ -66,7 +80,7 @@ $plannedWorkHours = CalendarItem::getPlannedWorkTimeByUserIdAndDate($pdo, $_SESS
     
     <div class="dashboard">
         <div class="bento-grid">
-            <div class="bento-item">
+            <div class="time-off bento-item">
                 <h2 class="bento-item-title">Request time off</h2>
                 <?php if (count($userRequests) > 0) : ?>
                     <?php foreach ($userRequests as $request) : ?>
@@ -91,82 +105,144 @@ $plannedWorkHours = CalendarItem::getPlannedWorkTimeByUserIdAndDate($pdo, $_SESS
                 <?php endif ?>
                 <button class="btn bento-item-button">Request time off</button>
             </div>
-        </div>
-        <div class="bento-item">
-            <h2 class="bento-item-title" id="time-tracker-title"><?php echo $timeTracker? "Clock Out" : "Clock In"; ?></h2>
-            <?php if ($plannedWorkHours["total_time"]) : ?>
-                <p id="plannedWorkHours">Planned work hours today: <?php echo $plannedWorkHours["total_time"] ?></p>
-            <?php else : ?>
-                <p id="plannedWorkHours">No planned work hours for today</p>
-            <?php endif ?>
-
-            <button class="btn bento-item-button" id="clockInButton" style="display: <?php echo $timeTracker? "none" : "block";  ?>">Start Work</button>
-            <button class="btn bento-item-button" id="clockOutButton" style="display: <?php echo $timeTracker && $timeTracker["end_time"] == null? "block" : "none";  ?>;">End Work</button>
-            
-            <div id="time-tracker-info">
-                <?php if ($timeTracker) : ?>
-                    <?php if ($timeTracker["end_time"] == null) : ?>
-                        <p id="clockInInfo">You clocked in at: <?php echo $timeTracker["start_time"] ?></p>
+            <div class="bento-item">
+                <h2 class="bento-item-title" id="time-tracker-title"><?php echo $timeTracker? "Clock Out" : "Clock In"; ?></h2>
+                <?php if ($plannedWorkHours["total_time"]) : ?>
+                    <p id="plannedWorkHours">Planned work hours today: <?php echo $plannedWorkHours["total_time"] ?></p>
+                <?php else : ?>
+                    <p id="plannedWorkHours">No planned work hours for today</p>
+                <?php endif ?>
+    
+                <button class="btn bento-item-button" id="clockInButton" style="display: <?php echo $timeTracker? "none" : "block";  ?>">Start Work</button>
+                <button class="btn bento-item-button" id="clockOutButton" style="display: <?php echo $timeTracker && $timeTracker["end_time"] == null? "block" : "none";  ?>;">End Work</button>
+                
+                <div id="time-tracker-info">
+                    <?php if ($timeTracker) : ?>
+                        <?php if ($timeTracker["end_time"] == null) : ?>
+                            <p id="clockInInfo">You clocked in at: <?php echo $timeTracker["start_time"] ?></p>
+                        <?php endif ?>
                     <?php endif ?>
+                </div>
+            </div>
+            <div class="myTasks">
+                <h2>My tasks</h2>
+                <?php if (count($myTasks) > 0) : ?>
+                    <ul>
+                        <?php foreach ($myTasks as $c) : ?>
+                            <li><?php echo $c["task"]?></li>
+                        <?php endforeach ?>
+                    </ul>
+                <?php endif ?>
+                <?php if (count($myTasks) == 0) : ?>
+                    <p>You don't have tasktypes</p>
                 <?php endif ?>
             </div>
-        </div>
-        <div class="myTasks">
-            <h2>My tasks</h2>
-            <?php if (count($myTasks) > 0) : ?>
-                <ul>
-                    <?php foreach ($myTasks as $c) : ?>
-                        <li><?php echo $c["task"]?></li>
+            <div class="sick-leave bento-item">
+                <h2 class="bento-item-title">Sick leave</h2>
+                <?php if (count($userSickLeave) > 0) : ?>
+                    <?php foreach ($userSickLeave as $sl) : ?>
+                        <div class="request">
+                            <p class="request-reason"><span class="request-label">Reason:</span> <?php echo $sl["reason"] ?></p>
+                            <p class="request-date-time"><span class="request-label">When:</span> 
+                                <?php
+                                    $start_date = date("F jS Y H:i", strtotime($sl["start_date"]));
+                                    $end_date = date("F jS Y H:i", strtotime($sl["end_date"]));
+                                    echo $start_date . " - " . $end_date;
+                                ?>
+                            </p>
+                        </div>
                     <?php endforeach ?>
-                </ul>
-            <?php endif ?>
-            <?php if (count($myTasks) == 0) : ?>
-                <p>You don't have tasktypes</p>
-            <?php endif ?>
+                <?php endif ?>
+                <?php if (count($userSickLeave) == 0) : ?>
+                    <p>You don't have active sick leave</p>
+                <?php endif ?>
+                <button class="btn bento-item-button">Call in sick</button>
+            </div>
         </div>
-        <div class="pop-up-overlay">
+        <div class="time-off pop-up-overlay">
             <div class="time-off-popup">
                 <button class="btn-close"><i class="fa fa-window-close-o"></i></button>
                 <form class="popup-form" action="" method="post">
+                    <input hidden type="text" name="typeOfForm" value="time-off">
                     <div class="form-column">
-                        <label for="reason">Reason for time off:</label>
-                        <input type="text" name="reason" id="reason" required placeholder="Reason">
+                        <label for="t-o-reason">Reason for time off:</label>
+                        <input type="text" name="reason" id="t-o-reason" required placeholder="Reason">
                     </div>
                     <div class="form-column">
-                        <label for="startdate">Start date and time:</label>
-                        <input type="datetime-local" name="startdate" id="startdate" required>
+                        <label for="t-o-startdate">Start date and time:</label>
+                        <input type="datetime-local" name="startdate" id="t-o-startdate" required>
                     </div>
                     <div class="form-column">
-                        <label for="enddate">End date and time:</label>
-                        <input type="datetime-local" name="enddate" id="enddate" required>
+                        <label for="t-o-enddate">End date and time:</label>
+                        <input type="datetime-local" name="enddate" id="t-o-enddate" required>
                     </div>
                     <div class="form-column">
-                        <label for="description">Description (not required):</label>
-                        <textarea name="description" id="description" cols="30" rows="5" placeholder="Give more info if necessary."></textarea>
+                        <label for="t-o-description">Description (not required):</label>
+                        <textarea name="description" id="t-o-description" cols="30" rows="5" placeholder="Give more info if necessary."></textarea>
                     </div>
                     <button class="btn btn-submit">Submit request</button>
+                </form>
+            </div>
+        </div>
+        <div class="sick-leave pop-up-overlay">
+            <div class="sick-leave-popup">
+                <button class="btn-close"><i class="fa fa-window-close-o"></i></button>
+                <form class="popup-form" action="" method="post">
+                    <input hidden type="text" name="typeOfForm" value="sick-leave">
+                    <div class="form-column">
+                        <label for="s-l-reason">Reason sick leave:</label>
+                        <input type="text" name="reason" id="s-l-reason" required placeholder="Reason">
+                    </div>
+                    <div class="form-column">
+                        <label for="s-l-startdate">Start date and time:</label>
+                        <input type="datetime-local" name="startdate" id="s-l-startdate" required>
+                    </div>
+                    <div class="form-column">
+                        <label for="s-l-enddate">End date and time:</label>
+                        <input type="datetime-local" name="enddate" id="s-l-enddate" required>
+                    </div>
+                    <button class="btn btn-submit">Submit sick leave</button>
                 </form>
             </div>
         </div>
     </div>
 
     <script>
-        const btnRequest = document.querySelector(".bento-item-button");
-        const popupOverlay = document.querySelector(".pop-up-overlay");
-        const popup = document.querySelector(".time-off-popup");
-        const btnClose = document.querySelector(".btn-close");
-        const btnSubmit = document.querySelector(".btn-submit");
+        const btnRequest_t_o = document.querySelector(".time-off .bento-item-button");
+        const popupOverlay_t_o = document.querySelector(".time-off.pop-up-overlay");
+        const popup_t_o = document.querySelector(".time-off-popup");
+        const btnClose_t_o = document.querySelector(".time-off-popup .btn-close");
+        const btnSubmit_t_o = document.querySelector(".time-off-popup .btn-submit");
 
         // Show time off request popup when button is clicked
-        btnRequest.addEventListener("click", () => {
-            popupOverlay.style.display = "block";
-            popup.style.display = "block";
+        btnRequest_t_o.addEventListener("click", () => {
+            popupOverlay_t_o.style.display = "block";
+            popup_t_o.style.display = "block";
         });
 
         // Close time off request popup when close button is clicked
-        btnClose.addEventListener("click", () => {
-            popupOverlay.style.display = "none";
-            popup.style.display = "none";
+        btnClose_t_o.addEventListener("click", () => {
+            popupOverlay_t_o.style.display = "none";
+            popup_t_o.style.display = "none";
+        });
+
+
+        const btnRequest_s_l = document.querySelector(".sick-leave .bento-item-button");
+        const popupOverlay_s_l = document.querySelector(".sick-leave.pop-up-overlay");
+        const popup_s_l = document.querySelector(".sick-leave-popup");
+        const btnClose_s_l = document.querySelector(".sick-leave-popup .btn-close");
+        const btnSubmit_s_l = document.querySelector(".sick-leave-popup .btn-submit");
+
+        // Show time off request popup when button is clicked
+        btnRequest_s_l.addEventListener("click", () => {
+            popupOverlay_s_l.style.display = "block";
+            popup_s_l.style.display = "block";
+        });
+
+        // Close time off request popup when close button is clicked
+        btnClose_s_l.addEventListener("click", () => {
+            popupOverlay_s_l.style.display = "none";
+            popup_s_s.style.display = "none";
         });
     </script>
     
